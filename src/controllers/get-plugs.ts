f927e5d1108dec6476942e0d27f27fa1d53262e4
AlineLermen <aline.helena.lermen@gmail.com>
2023-02-01 19:13:32 -0300

    Corrige get-plugs

--- src/controllers/get-plugs.ts
+++ src/controllers/get-plugs.ts
@@ -1,11 +1,8 @@
-import { User } from '../database/entities/domain/user'
-import { GetUserResponse } from '../usecases/get-user/domain/get-user-response'
-import { GetUserError } from '../usecases/get-user/errors/get-user-error'
 import { GetPlugsUseCase } from '../usecases/get-plugs/use-case'
 import { Controller } from './domain/controller'
 import { badRequest, HttpResponse, serverError, success } from './helpers/http'
-import { GetPlugsResponse } from '../usecases/get-plugs/domain/get-plugs-response'
 import { Plug } from '../database/entities/plug'
+import { GetPlugError } from '../usecases/set-plug/errors/get-plug-error'
 
 type HttpRequest = {
     inUse?: boolean
@@ -22,7 +19,7 @@ export class GetPlugsController extends Controller {
     const response = await this.getPlugs.execute(params)
     if (response.isSuccess && response.body) {
       return success(response.body)
-    } else if (!response.isSuccess && response.error instanceof GetUserError) {
+    } else if (!response.isSuccess && response.error instanceof GetPlugError) {
       return badRequest(response.error)
     } else {
       return serverError(response.error)
--- src/usecases/get-plugs/errors/get-plugs-error.ts
+++
@@ -1,6 +0,0 @@
-export class GetPlugsrError extends Error {
-    constructor() {
-      super('Nenhuma tomada com esses requisitos foi encontrada')
-      this.name = 'GetPlugsrError'
-    }
-  }
