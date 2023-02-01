c0b11a256f16408da5dc90572033a17b6bf389a7
DaviMarinho <davii_marinho@hotmail.com>
2023-01-31 22:07:28 -0300

    Adiciona os enviroments do banco para o deploy
    Signed-off-by: DaviMarinho <davii_marinho@hotmail.com>

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,15 +3,16 @@ import path from 'path'
 
 const connectOptions: DataSourceOptions = {
   type: 'postgres',
-  // host: 'localhost',
-  url: process.env.DB_URL,
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  // database: 'user',
+  host: process.env.HOST,
+  url: process.env.DATABASE_URL,
+  port: Number(process.env.DB_PORT),
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   synchronize: true,
   entities: [`${path.join(__dirname, '/entities/*{.ts, .js}')}`],
-  migrations: [`${path.join(__dirname, '/migrations/*{.ts, .js}')}`]
+  migrations: [`${path.join(__dirname, '/migrations/*{.ts, .js}')}`],
+  migrationsRun: true
 }
 
 export const dataSource = new DataSource(connectOptions)
